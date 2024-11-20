72f7b1120e4140d4a28c6fb8dbcf078d19e8b28e
laoxie72 <744286652@qq.com>
2024-11-20 09:31:03 +0800

    add simple perdiction for my next expense

--- expenseAnalysis.py
+++ expenseAnalysis.py
@@ -36,6 +36,21 @@ try:
             # 绘制拟合曲线
             plt.plot(indices, fitted_values, color='red', label='curve')
 
+            # 预测下一个消费金额
+            next_index = len(indices)  # 下一个数据点的索引
+            next_value = polynomial(next_index)  # 使用模型预测值
+            print(f"预测的下一个消费金额: {next_value:.2f}")
+
+            # 标注预测值在图表中
+            plt.scatter([next_index], [next_value], color='green', label='预测值', zorder=5)
+            plt.annotate(f"{next_value:.2f}",
+                         (next_index, next_value),
+                         textcoords="offset points",
+                         xytext=(5, 10),
+                         ha='center',
+                         fontsize=10,
+                         color='green')
+
             # 图表设置
             plt.xlabel('data Index')
             plt.ylabel('Expense')
@@ -44,7 +59,7 @@ try:
             plt.grid(True)
 
             # 保存图片为 PNG 文件
-            plt.savefig("scatter_fit.png", format="png", dpi=300, bbox_inches="tight")
+            # plt.savefig("scatter_fit.png", format="png", dpi=300, bbox_inches="tight")
 
             # 显示图表
             plt.show()
